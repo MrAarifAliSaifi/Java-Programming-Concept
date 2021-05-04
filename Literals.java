1308a977f2207d574c37ebb03da32f4b4a0bafc3
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-05-04 10:04:29 -0700

    Literals i]n java

---
+++ Literals.java
@@ -0,0 +1,10 @@
+public class Literals {
+    public static void main(String[] args) {
+        //in java literal is any constant value which can be assign to the variable
+        //literals may any type like int literral char literals float literals etc
+        int var=10;//here in this line 10 is a literal
+        char ch='a';//here a is a literals
+        float var1=12.22f;//here 12.22 is also a literal and so on
+
+    }
+}
