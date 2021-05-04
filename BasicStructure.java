416762b2937d716a4f9b9c4f29deb9376491b2c8
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-05-04 09:19:10 -0700

    Add files via upload

---
+++ BasicStructure.java
@@ -0,0 +1,9 @@
+public class BasicStructure {
+    public static void main(String[] args) {
+        System.out.println("System is workig properlya");//this method which is used to print the line same as it
+        System.out.print("In this manner we can also print the statements");
+        System.out .printf("This is also a method to print the things");
+        System.out.format("hello this is also here ");
+    }
+    }
+
