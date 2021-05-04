cec7179995280b3f7a54b3f4bfcf0b960f84c79a
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-05-04 10:55:53 -0700

    Operator associativity and presedence

---
+++ OperatorprecedenceAndAssociativity.java
@@ -0,0 +1,8 @@
+public class OperatorprecedenceAndAssociativity {
+    public static void main(String[] args) {
+        //in programming is there is any mathematical exprresion is given like 5*6-12+34/2 so to  solve this type od expression we used precidence and associativity
+        int val=5*6-12+34/2;
+        System.out.println("the result of this expresiion is:"+val);
+        //here we first * then/then+and in lthe last we use - in the last
+    }
+}
