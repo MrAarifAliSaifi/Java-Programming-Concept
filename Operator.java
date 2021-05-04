9c3e5de39755b258aa395476e8807d624c8eb52d
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-05-04 10:29:15 -0700

    Operator in java

---
+++ Operator.java
@@ -0,0 +1,30 @@
+public class Operator {
+    public static void main(String[] args){
+        //in java  there are many operator available as we have study in mathematics already which are used to perform a mathematical operation
+        //in java there are type of operator avalable
+        //first type of operator is "ARITHMATICS OPERATOR";
+        int a=5;
+        int b=10;
+        int c=a+b;//+ is a arithmatics operator
+        int d=b-a;//-is also
+        int e=a*b;//* is also
+        int f=b/a;//is also
+        float fl=a%b;//is also
+        //second type of operater available in java is
+        //Assignment operator
+        int k=10;
+        int m=k;//here we are assigning the value of k in to m using the operator =k+=1
+        k+=1;//here we are incresing the value  of the of the variable k by 1
+        k-=1;
+        //Third type of operator in java is increment and decrement
+        k+=1;//here we are incresing the value  of the of the variable k by 1
+        k-=1;
+        // Logiacl operator
+        //&&,||,!= etc
+        //Bitwise oprerator
+
+
+
+
+    }
+}
