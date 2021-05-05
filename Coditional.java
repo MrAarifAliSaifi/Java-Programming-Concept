afa286791588b4f0bbeb6f394954fd36b659cf34
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-05-05 04:51:33 -0700

    if-else statements

---
+++ Coditional.java
@@ -0,0 +1,15 @@
+package com.company;
+public class Coditional{
+    public static void main(String[] args){
+        //condition statements used where we want to take the decision
+        //for example
+        int age=17;//here we are decalring the age
+        if( age>=18)//here we are taking the decission
+        {
+           System.out.println("You can vote!");//here wea re printing the statments
+        }
+        else
+            System.out.println("You can not vote!");
+
+    }
+}
