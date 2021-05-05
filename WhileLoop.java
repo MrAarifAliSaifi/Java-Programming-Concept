253c611213ce0b860a3ed082fd455b07f2e77cf9
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-05-05 05:16:50 -0700

    while loop in java

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+package com.company;
+public class WhileLoop {
+    //if we want to print the value from 1 to 10 suing while lopp in java
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
