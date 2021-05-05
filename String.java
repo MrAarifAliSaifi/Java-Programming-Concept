61da000a1c2c7ddcc40f8a023063e8681113219e
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-05-05 04:26:22 -0700

    String in java

---
+++ String.java
@@ -0,0 +1,6 @@
+public class String {
+    public static void main(String[] args) {
+        String str=new String();//here we are creating the object of the string class
+        //we can directly also can acces by using like data type string 
+    }
+}
