707602cf105c25a84d539b1de2cf468532ffb7fa
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-05-05 05:13:16 -0700

    Forloop in java

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Forloop {
+    public static void main(String[] args) {
+        System.out.println("here we are using the concept of the foor loop");
+        //if we want print the number fom 1 to 10
+        //so we will use for loop here
+        for(int i=1;i<10;i++)
+        {
+            System.out.println(i);
+
+        }
+    }
+}
