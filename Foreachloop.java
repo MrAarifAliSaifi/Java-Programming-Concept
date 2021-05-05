013a7ec1368b7fd6ebbab9b1e18ab427fe77ca43
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-05-05 09:48:52 -0700

    Foreachloop in java

---
+++ Foreachloop.java
@@ -0,0 +1,12 @@
+package com.company;
+public class Foreachloop {
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int arr[]= ints;
+        for(int elements:arr)
+        {
+            System.out.println(elements);
+        }
+
+    }
+}
